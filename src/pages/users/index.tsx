ee340e5d49ede04d0c10b2eb911dbf3395ab83c9
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-03-15 11:56:02 -0300

    feat(users): users list ok

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
-import { RiAddLine } from "react-icons/ri";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
@@ -23,10 +23,60 @@ export default function UserList() {
                             size="sm" 
                             fontSize="sm" 
                             colorScheme="pink"
-                            leftIcon={<Icon as={RiAddLine} />}>
+                            leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
                                 Criar novo
                         </Button>
                     </Flex>
+
+                    <Table
+                        colorScheme="whiteAlpha"
+                    >
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>
+                                    Usuário
+                                </Th>                                
+                                <Th>
+                                    Data de cadastro
+                                </Th> 
+                                <Th width="8"></Th>                               
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td px="6">
+                                    <Box>
+                                        <Text fontWeight="bold">
+                                            Fernando Silva
+                                        </Text>
+                                        <Text fontSize="small" color="gray.3000">
+                                            fernandosilva.smsrio@gmail.com
+                                        </Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    04 de Abril, 2021
+                                </Td>
+                                <Td>
+                                <Button 
+                                    as="a" 
+                                    size="sm" 
+                                    fontSize="sm" 
+                                    colorScheme="purple"
+                                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                                        Editar
+                                </Button>                                    
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+
                 </Box>
             </Flex>
         </Box>
