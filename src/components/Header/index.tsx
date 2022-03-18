942980b9c207af7f5eaa113219f951eb3a3540d5
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-03-18 13:12:22 -0300

    feat: responsive sidebar menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import { 
-    Flex, useBreakpointValue,
+    Flex, Icon, IconButton, useBreakpointValue,
 } from "@chakra-ui/react";
 
 import NotificationsNav from "./NotificationsNav";
 import Profile from "./Profile";
 import SearchBox from "./SearchBox";
 import Logo from "./Logo";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export default function Header() {
+
+    const { onOpen } = useSidebarDrawer()
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -25,6 +29,20 @@ export default function Header() {
             px="6"
             align="center"
         >
+
+            { !isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+
+                </IconButton>
+            ) }
+
             <Logo />
 
             { isWideVersion && <SearchBox /> } 
