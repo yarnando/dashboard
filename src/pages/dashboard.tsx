0d1420b77523f5f8448831200718781dd7ea3eb4
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-03-18 13:33:25 -0300

    style(dashboard): misc style

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -74,7 +74,7 @@ export default function Dashboard() {
                 <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignContent="flex-start">
 
                     <Box
-                        p="8"
+                        p={["6", "8"]}
                         bg="gray.800"
                         borderRadius={8}
                         pb="4"
@@ -89,7 +89,7 @@ export default function Dashboard() {
                     </Box>
 
                     <Box
-                        p="8"
+                        p={["6", "8"]}
                         bg="gray.800"
                         borderRadius={8}
                         pb="4"
