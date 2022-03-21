fad9197cf7d68ef52992ee8557c332d1697b47c8
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-03-21 20:21:57 -0300

    feat: react form refs

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,13 +4,14 @@ import {
     Input as ChakraInput,
     InputProps as ChakraInputProps
 } from '@chakra-ui/react'
+import { forwardRef, ForwardRefRenderFunction } from 'react';
 
 interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }: InputProps, ref) => {
     return (
         <FormControl>
 
@@ -28,9 +29,12 @@ export function Input({ name, label, ...rest }: InputProps) {
                     bgColor: 'gray.900'
                 }}
                 size="lg"
+                ref={ref}
                 {...rest}
             />
 
         </FormControl>
     );
 }
+
+export const Input = forwardRef(InputBase)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import {
   Flex,
   FormControl,
-  FormLabel,
   Button,
   Stack
 } from '@chakra-ui/react'
 
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import { Input } from '../components/Form/Input'
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+
+  const { register, handleSubmit } = useForm()
+
+  const handleSignIn: SubmitHandler<SignInFormData> = (values) => {
+    console.log(values);
+  }
+
   return (
     <Flex
       w="100vw"
@@ -24,6 +37,7 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
 
@@ -33,6 +47,7 @@ export default function SignIn() {
               name="email"
               type="email"
               label="E-mail"
+              {...register('email')}
             />
 
           </FormControl>
@@ -43,6 +58,7 @@ export default function SignIn() {
               name="password"
               type="password"
               label="Senha"
+              {...register('password')}
             />            
 
           </FormControl>
