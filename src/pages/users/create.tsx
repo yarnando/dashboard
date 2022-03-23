faa88a84e9e0e6dcae6d6809fa75c0a83a149967
Fernando Silva <fernandosilva.smsrio@gmail.com>
2022-03-23 12:59:57 -0300

    feat(users): name/email being validated

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -16,7 +16,7 @@ type createUserFormData = {
     password: string;
     password_confirmation: string;
 }
-  
+   
 const createUserFormSchema = yup.object().shape({
     name: yup.string().required('Nome obrigatório'),
     email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
